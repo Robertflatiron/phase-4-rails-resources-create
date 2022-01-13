74af79f8aacd0d0f175c5f6ee3348283863431af
Robert Gaspar <gaspar.robert7@gmail.com>
2022-01-13 17:18:41 -0600

    commit tree f3c1d5f19eb3fcecaaa44b7eee1e06cafa2eea95

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,13 @@
 class BirdsController < ApplicationController
 
+  def create
+    # get the data from the body of the request
+    # create a new Bird
+    bird = Bird.create(name: params[:name], species: params[:species])
+    # send the newly create Bird as a response
+    render json: bird, status: :created
+  end
+
   # GET /birds
   def index
     birds = Bird.all
@@ -15,5 +23,6 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
 
 end
